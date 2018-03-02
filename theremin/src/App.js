f67672a9fa2ba9963e530d3bf4ac2fc8273e19b4
Michael Norberg <mnorberg88@gmail.com>
2018-03-02 14:37:38 -0500

    theremin is working

--- theremin/src/App.js
+++ theremin/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      xAxis: 0,
+      yAxis: 0,
+    };
+  };
+  componentDidMount() { 
+    this.props.oscillator.connect(this.props.gain);
+    this.props.gain.connect(this.props.output)
+
+    let initialFreq = 1000;
+    let initialVol = 0.1;
+    //oscillator
+    this.props.oscillator.detune.value = 100;
+    this.props.oscillator.frequency.value = initialFreq
+    this.props.oscillator.start(0); 
+    this.props.gain.gain.value = initialVol
+  }
+  componentWillMount() {
+    document.addEventListener('mousemove', this.playTheremin);
+  }
+  playTheremin = (e) => {
+    console.log('you hit it')
+    let xAxis = this.state.xAxis; 
+    let yAxis = this.state.xAxis;
+    xAxis = (e) ? e.pageX : e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+    yAxis = (e) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop); 
+    this.props.oscillator.frequency.value = (xAxis/this.props.width) * this.props.maxFreq
+    this.props.gain.gain.value =(yAxis/this.props.height) * this.props.maxVol
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        
       </div>
     );
   }
--- theremin/src/index.js
+++ theremin/src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioCtx = new AudioContext();
+let oscillator = audioCtx.createOscillator();
+let gain = audioCtx.createGain();
+let output = audioCtx.destination;
+let width = window.innerWidth;
+let height = window.innerHeight;
+let maxFreq = 6000;
+let maxVol = 0.02;
+
+ReactDOM.render(<App oscillator={oscillator} 
+                     gain={gain} 
+                     output={output} 
+                     width={width} 
+                     height={height}
+                     maxFreq={maxFreq}
+                     maxVol={maxVol}/>, document.getElementById('root'));
 registerServiceWorker();
